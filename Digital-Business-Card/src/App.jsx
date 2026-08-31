10c53f47b340a64c2ec312ed60b304ac4c748c13
kylemucha <kylemuchaswe@gmail.com>
2026-08-31 11:38:32 -0700

    more progress on digital business card

--- Digital-Business-Card/src/App.jsx
+++ Digital-Business-Card/src/App.jsx
@@ -5,11 +5,11 @@ import Footer from "./components/Footer.jsx"
 
 export default function App () {
     return (
-        <>
+        <main className="card">
             <Info />
             <About />
             <Interests />
             <Footer />
-        </>
+        </main>
     )
 }
--- Digital-Business-Card/src/components/Info.jsx
+++ Digital-Business-Card/src/components/Info.jsx
@@ -1,8 +1,23 @@
+import headshot from "../pictures/kylemucha-headshot.png"
+import emailButton from "../pictures/email-button.png"
+import linkedinButton from "../pictures/linkedin-button.png"
 export default function Info () {
     return (
-        <header>
-            <img className="headshot" src="src/pictures/kylemucha-headshot.png" />
-            <h1>Kyle Mucha</h1>
-        </header>
+        <>
+            <header>
+                <img className="headshot" src={headshot} alt="Picture of Kyle Mucha" />
+                <h1>Kyle Mucha</h1>
+                <p className="job-title">Future Software Engineer</p>
+                <a className="website-link" href="#" target="_blank">kylemucha.about</a>
+            </header>
+            <div className="button-row">
+                <a className="button" href="mailto:kylemuchaswe@gmail.com">
+                        <img src={emailButton}></img>
+                </a>
+                <a className="button" href="https://www.linkedin.com/in/kylemuchaa/" target="_blank">
+                        <img src={linkedinButton}></img>
+                </a>
+            </div>
+        </>
     )
 }
