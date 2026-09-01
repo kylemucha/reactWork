74993e1fb256e6f527911a8125d02d309c81a741
kylemucha <kylemuchaswe@gmail.com>
2026-09-01 16:52:22 -0700

    added header with correct font and spacing

--- TravelJournal/src/App.jsx
+++ TravelJournal/src/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./components/Header"
+
 export default function App () {
   return (
-    <h1>Travel my bunghole</h1>
+    <>
+      <Header />
+    </>
   )
 }
---
+++ TravelJournal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return(
+        <header>
+            <img className="header-globe"src="src/pictures/tinyGlobe.png" alt="Globe Icon"></img>
+            <p>my travel journal.</p>
+        </header>
+    )
+}
