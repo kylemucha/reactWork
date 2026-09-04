3f0b3412cc41aef86efd718691566ce7d41d771a
kylemucha <kylemuchaswe@gmail.com>
2026-09-03 19:54:10 -0700

    some fixes

--- TravelJournal/src/App.jsx
+++ TravelJournal/src/App.jsx
@@ -2,24 +2,16 @@ import Header from "./components/Header"
 import Entry from "./components/Entry"
 import Data from "./data"
 
-/**
- * Challenge:
- * - map over the array to create an <Entry /> component
- *   for every item in the data array.
- * - display the array of Entry components in place of the current
- *   hard-coded <Entry /> instance.
- */
-
 export default function App () {
 
   const locationEntries = Data.map((location) => {
     return (
-      <>
-    <Entry 
+    <>
+      <Entry 
       img={location.img}
       title={location.title}
       country={location.country}
-      googleMapsLink={location.country}
+      googleMapsLink={location.googleMapsLink}
       dates={location.dates}
       text={location.text}
     />
