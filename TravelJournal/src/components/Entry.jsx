8fe0769b2ff3e630b88ba972eedc241dddd46531
kylemucha <kylemuchaswe@gmail.com>
2026-09-03 18:22:49 -0700

    Removed hardcoded data from component and utilized the use of props to pass data

--- TravelJournal/src/components/Entry.jsx
+++ TravelJournal/src/components/Entry.jsx
@@ -1,16 +1,18 @@
-export default function Entry () {
+import marker from "../pictures/marker.png"
+
+export default function Entry (props) {
     return (
         <div className="location-entry">
-            <img className="location-picture" src="https://scrimba.com/links/travel-journal-japan-image-url"></img>
+            <img className="location-picture" src={props.img.src} alt={props.img.alt}></img>
             <div className="location-information">
-                <img className="location-marker" src="src/pictures/marker.png"></img>
-                <span>J A P A N</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+                <img className="location-marker" src={marker} alt="Map Marker Icon"></img>
+                <span>{props.country}</span>
+                <a href={props.googleMapsLink}
                 target="_blank"
                 >View on Google Maps</a>  
-                <h2>Mount Fuji</h2>
-                <p className="location-dates">12 Jan, 2021 - 24 Jan, 2021</p>   
-                <p className="location-description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>    
+                <h2>{props.title}</h2>
+                <p className="location-dates">{props.dates}</p>   
+                <p className="location-description">{props.text}</p>    
             </div>
         </div>
     )
