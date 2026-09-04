5c99717b1f08668d67d4a0947fdaa9ed6e186ac8
kylemucha <kylemuchaswe@gmail.com>
2026-09-04 16:06:22 -0700

    added some functions to handle collecting list of ingredients. Not fully developed yet

--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -1,9 +1,23 @@
 export default function Main () {
+
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsList = ingredients.map((ing) => 
+        <li key={ing}>{ing}</li>
+    )
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient") //matches name of input on form
+    }
+
     return (
     <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
                 name="ingredient" 
+                type="text"
                 placeholder="e.g. oregano" 
                 aria-label="Add ingredient"
             />
