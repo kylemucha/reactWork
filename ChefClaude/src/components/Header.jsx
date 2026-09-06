1ee2be32515cd99d65a444a065e5e41888cba454
kylemucha <kylemuchaswe@gmail.com>
2026-09-06 14:32:41 -0700

    added alt to Chef Claude Header icon

--- ChefClaude/src/components/Header.jsx
+++ ChefClaude/src/components/Header.jsx
@@ -3,7 +3,7 @@ import chefClaude from "../images/chefClaude.png"
 export default function Header () {
     return (
         <header>
-            <img className="chef-claude-icon" src={chefClaude}/>
+            <img className="chef-claude-icon" src={chefClaude} alt="Icon of a robot chef"/>
             <h1>Chef Claude</h1>
         </header>
     )
