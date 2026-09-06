d0bf5b3998fb0297ead6e8187aece90a2dacf2a4
kylemucha <kylemuchaswe@gmail.com>
2026-09-06 14:31:23 -0700

    alt name changes

--- ReactFacts/src/components/Main.jsx
+++ ReactFacts/src/components/Main.jsx
@@ -9,7 +9,7 @@ export default function Main() {
                 <li>Is maintained by Meta</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-            <img className="half-react-logo" src="src/pictures/half-react-logo.png" alt="React Logo cut in Half"/>
+            <img className="half-react-logo" src="src/pictures/half-react-logo.png" alt="React logo with Right half cut off right side of the screen"/>
         </main>
     )
 }
