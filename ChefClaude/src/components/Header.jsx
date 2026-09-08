7862308104bbe77c1ac8f6c3287d288c34c21dd9
kylemucha <kylemuchaswe@gmail.com>
2026-09-08 02:05:47 -0700

    some fixes

--- ChefClaude/src/components/Header.jsx
+++ ChefClaude/src/components/Header.jsx
@@ -3,7 +3,7 @@ import chefClaude from "../images/chefClaude.png"
 export default function Header () {
     return (
         <header>
-            <img className="chef-claude-icon" src={chefClaude} alt="Chef Claude Icon"/>
+            <img className="chef-claude-icon" src={chefClaude} alt="Chef Claude Tiny Chef Icon"/>
             <h1>Chef Claude</h1>
         </header>
     )
