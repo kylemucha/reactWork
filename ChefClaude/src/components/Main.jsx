785cc9458dc8e5c38e4b0c60cea58312d4815396
kylemucha <kylemuchaswe@gmail.com>
2026-09-09 21:28:01 -0700

    fixed code styling

--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -13,20 +13,20 @@ export default function Main () {
     }
 
     return (
-    <div className="main-content">
-        <form onSubmit={handleSubmit}>
-            <input 
-                name="ingredient" 
-                type="text"
-                placeholder="e.g. oregano" 
-                aria-label="Add ingredient"
-            />
-            <button className="add-ingredient-button" type="submit">+ Add Ingredient</button> 
-        </form>
+        <div className="main-content">
+            <form onSubmit={handleSubmit}>
+                <input 
+                    name="ingredient" 
+                    type="text"
+                    placeholder="e.g. oregano" 
+                    aria-label="Add ingredient"
+                />
+                <button className="add-ingredient-button" type="submit">+ Add Ingredient</button> 
+            </form>
             <h2>Ingredients on Hand:</h2>
             <ul>
                 {ingredientsList}
             </ul>
-    </div>
+        </div>
     )
 }
