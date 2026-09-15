cc49af50c8bace5d8e1f440fc0e0d8227df26206
kylemucha <kylemuchaswe@gmail.com>
2026-09-15 16:53:25 -0700

    formatting

--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -1,33 +1,34 @@
-import React from "react"
-export default function Main () {
+import React from "react";
+export default function Main() {
+  const [ingredients, setIngredients] = React.useState([
+    "banana",
+    "apple",
+    "jacks",
+  ]);
 
-    const [ingredients, setIngredients] = React.useState(["banana", "apple", "jacks"])
+  const ingredientsList = ingredients.map((ing) => <li key={ing}>{ing}</li>);
 
-    const ingredientsList = ingredients.map(ing => (
-        <li key={ing}>{ing}</li>
-    ))
+  // Handles getting form data from user input to add new ingredient to array of ingredients
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+  }
 
-    // Handles getting form data from user input to add new ingredient to array of ingredients
-    function addIngredient(formData) {
-        const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredient => [...prevIngredient, newIngredient])
-    }
-
-    return (
-        <div className="main-content">
-            <form action={addIngredient}>
-                <input 
-                    name="ingredient" 
-                    type="text"
-                    placeholder="e.g. oregano" 
-                    aria-label="Add ingredient"
-                />
-                <button className="add-ingredient-button" type="submit">+ Add Ingredient</button> 
-            </form>
-            <h2>Ingredients on Hand:</h2>
-            <ul>
-                {ingredientsList}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="main-content">
+      <form action={addIngredient}>
+        <input
+          name="ingredient"
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+        />
+        <button className="add-ingredient-button" type="submit">
+          + Add Ingredient
+        </button>
+      </form>
+      <h2>Ingredients on Hand:</h2>
+      <ul>{ingredientsList}</ul>
+    </div>
+  );
 }
