88031e5af49c74fa5f6409bd66a2222b6c7c99d9
kylemucha <kylemuchaswe@gmail.com>
2026-09-21 11:35:47 -0700

    added comments

--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -51,7 +51,7 @@ export default function Main() {
         <h2>Chef Claude Recommends:</h2>
         <article className="suggested-recipe-container" aria-live="polite">
 
-          // sample recipe. Delete after Anthropic API implementation
+          // sample recipe. Delete after Anthropic API implementation is finished
           <p>
             Based on the ingredients you have available, I would recommend
             making a simple a delicious <strong>Beef Bolognese Pasta</strong>.
