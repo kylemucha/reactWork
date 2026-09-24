6e6e68dcbebb2428211d35604ae57d40a694e2be
kylemucha <kylemuchaswe@gmail.com>
2026-09-24 12:03:38 -0700

    Moved the IngredientsList and associating logic into its own seperate component

--- ChefClaude/src/components/IngredientsList.jsx
+++ ChefClaude/src/components/IngredientsList.jsx
@@ -1,6 +1,47 @@
-export default function IngredientsList() {
-    return (
-        <>
-        </>
-    )
+import React from "react"
+export default function IngredientsList(props) {
+
+
+  const [ingredients, setIngredients] = React.useState([]);
+  const ingredientsList = ingredients.map((ing) => <li key={ing}>{ing}</li>);
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+  }
+
+  return (
+    <div>
+      <form action={addIngredient}>
+        <input
+          name="ingredient"
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+        />
+        <button className="add-ingredient-button" type="submit">
+          + Add Ingredient
+        </button>
+      </form>
+      {ingredients.length > 0 ? (
+        <div>
+          <h2>Ingredients on Hand:</h2>
+          <ul>{ingredientsList}</ul>
+        </div>
+      ) : undefined}
+      {ingredients.length > 3 ? (
+        <section className="generate-recipe-section">
+          <div className="generate-recipe-text">
+            <p className="generate-recipe-toptext">Ready for a recipe?</p>
+            <p className="generate-recipe-bottomtext">
+              Generate a recipe from your list of ingredients.
+            </p>
+          </div>
+          <button onClick={props.generateRecipe} className="generate-recipe-button">
+            Get a recipe
+          </button>
+        </section>
+      ) : undefined}
+    </div>
+  );
 }
--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -1,16 +1,10 @@
 import React from "react"
 import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientsList";
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState([]);
   const [recipeShown, setRecipeShown] = React.useState(false);
 
-  const ingredientsList = ingredients.map((ing) => <li key={ing}>{ing}</li>);
-
   // Handles getting form data from user input to add new ingredient to array of ingredients
-  function addIngredient(formData) {
-    const newIngredient = formData.get("ingredient");
-    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
-  }
 
   //function handles flipping the boolean which handles showing the recipe (temporary before adding Anthropic API functionality)
   function generateRecipe() {
@@ -19,36 +13,7 @@ export default function Main() {
 
   return (
     <div className="main-content">
-      <form action={addIngredient}>
-        <input
-          name="ingredient"
-          type="text"
-          placeholder="e.g. oregano"
-          aria-label="Add ingredient"
-        />
-        <button className="add-ingredient-button" type="submit">
-          + Add Ingredient
-        </button>
-      </form>
-      {ingredients.length > 0 ? (
-        <div>
-          <h2>Ingredients on Hand:</h2>
-          <ul>{ingredientsList}</ul>
-        </div>
-      ) : null}
-      {ingredients.length > 3 ? (
-        <section className="generate-recipe-section">
-          <div className="generate-recipe-text">
-            <p className="generate-recipe-toptext">Ready for a recipe?</p>
-            <p className="generate-recipe-bottomtext">
-              Generate a recipe from your list of ingredients.
-            </p>
-          </div>
-          <button onClick={generateRecipe} className="generate-recipe-button">
-            Get a recipe
-          </button>
-        </section>
-      ) : null}
+      <IngredientsList generateRecipe={generateRecipe}/>
       {recipeShown ? (
         <section>
           <h2>Chef Claude Recommends:</h2>
